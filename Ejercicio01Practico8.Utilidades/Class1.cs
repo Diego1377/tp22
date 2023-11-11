50d5f73a9e7b55f42e929d972bf96612e3cbca61
diego biancofiore <diegobiancofiore23@gmail.com>
2023-11-11 15:34:14 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01Practico8.Utilidades/Class1.cs
@@ -0,0 +1,99 @@
+﻿namespace Ejercicio01Practico8.Utilidades
+{
+    public static class IngresoDatos
+    {
+        public static string PedirCharEnRango(string mensaje, char char1, char char2)
+        {
+            char cX;
+            do
+            {
+                Console.Write(mensaje);
+                var tecla = Console.ReadKey();
+                if (tecla.KeyChar.ToString().ToUpper() == char1.ToString().ToUpper() ||
+                    tecla.KeyChar.ToString().ToUpper() == char2.ToString().ToUpper())
+                {
+                    cX = tecla.KeyChar;
+                    break;
+                }
+                Console.WriteLine("Tecla Ingresada no valida!!!");
+            } while (true);
+            return cX.ToString().ToUpper();
+        }
+        public static string PedirString(string mensaje)
+        {
+            string? cX;
+            do
+            {
+                Console.Write(mensaje);
+                cX = Console.ReadLine();
+                if (!string.IsNullOrEmpty(cX) || !string.IsNullOrEmpty(cX))
+                {
+                    break;
+                }
+                Console.WriteLine("No ingreso nada por la consola");
+            } while (true);
+            return cX;
+        }
+        public static int PedirInt(string mensaje)
+        {
+            int nro;
+            string cX;
+            do
+            {
+                cX = PedirString(mensaje);
+                if (int.TryParse(cX, out nro))
+                {
+                    break;
+                }
+                Console.WriteLine("Numero no Valido");
+            } while (true);
+            return nro;
+        }
+        public static int PedirIntEnRango(string mensaje, int valorMenor, int valorMayor)
+        {
+            bool error = true;
+            int valorInt;
+            string? cX;
+            do
+            {
+                cX = PedirString(mensaje);
+                if (!int.TryParse(cX, out valorInt))
+                {
+                    Console.WriteLine("Error al intentar ingresar un valor entero");
+                }
+                else if (valorInt < valorMenor || valorInt > valorMayor)
+                {
+                    Console.WriteLine($"ERROR valor fuera del rango perimitido {valorMenor}-{valorMayor} ");
+                }
+                else
+                {
+                    error = false;
+                }
+            } while (error);
+            return valorInt;
+        }
+        public static double PedirDoubleEnRango(string mensaje, Double valorMenor, Double valorMayor)
+        {
+            bool error = true;
+            double valorDouble;
+            string? cX;
+            do
+            {
+                cX = PedirString(mensaje);
+                if (!double.TryParse(cX, out valorDouble))
+                {
+                    Console.WriteLine("Error al intentar ingresar un valor entero");
+                }
+                else if (valorDouble < valorMenor || valorDouble > valorMayor)
+                {
+                    Console.WriteLine($"ERROR valor fuera del rango perimitido {valorMenor}-{valorMayor} ");
+                }
+                else
+                {
+                    error = false;
+                }
+            } while (error);
+            return valorDouble;
+        }
+    }
+}
